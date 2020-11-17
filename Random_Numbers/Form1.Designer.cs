32143fae4db4afd79a4301b1828b4df9351e7021
NavienVinz26 <67771480+NavienVinz26@users.noreply.github.com>
2020-11-17 23:24:25 +0800

    Add files via upload
    It's a random number game.

---
+++ Random_Numbers/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace labtest5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.ListBox();
+            this.lb2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.FormattingEnabled = true;
+            this.lb1.ItemHeight = 16;
+            this.lb1.Location = new System.Drawing.Point(12, 37);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(205, 500);
+            this.lb1.TabIndex = 0;
+            this.lb1.SelectedIndexChanged += new System.EventHandler(this.lb1_SelectedIndexChanged);
+            // 
+            // lb2
+            // 
+            this.lb2.FormattingEnabled = true;
+            this.lb2.ItemHeight = 16;
+            this.lb2.Location = new System.Drawing.Point(263, 37);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(218, 500);
+            this.lb2.TabIndex = 1;
+            this.lb2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numbers to choose from";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Guesses made per round";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Location = new System.Drawing.Point(576, 220);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(114, 38);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play Again";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Enabled = false;
+            this.btnExit.Location = new System.Drawing.Point(576, 282);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(114, 38);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lb3
+            // 
+            this.lb3.AutoSize = true;
+            this.lb3.Location = new System.Drawing.Point(536, 63);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(0, 17);
+            this.lb3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 560);
+            this.Controls.Add(this.lb3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.ListBox lb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lb3;
+    }
+}
+
---
+++ Random_Numbers/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labtest5
+{
+    public partial class Form1 : Form
+    {
+        int guesses = 0;
+        List<Int32> ListNumber = new List<Int32>();
+        int secretNumber = 0, index = 0, chosen = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            guesses = 0;
+            chosen = 0;
+            lb3.Text = "";
+            ListNumber.Clear();
+            lb1.Items.Clear();
+            for(int i=1;i<50;i++)
+            {
+                ListNumber.Add(i);
+                lb1.Items.Add(i);
+            }
+            btnPlay.Enabled = false;
+            btnExit.Enabled = false;
+            lb1.Enabled = true;
+
+            Random rand = new Random();
+            secretNumber = rand.Next(1, 51);
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 51; i++)
+            {
+                ListNumber.Add(i);
+                lb1.Items.Add(i);
+            }
+            Random rand = new Random();
+            secretNumber = rand.Next(1, 51);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            foreach(int item in lb2.Items)
+            {
+                sum = guesses;
+            }
+            double avg = sum / lb2.Items.Count;
+
+            MessageBox.Show("You played" + lb2.Items.Count + "rounds" + "Your average is" + avg +"Per round.\n" + "Thank you for playing##");
+            Application.Exit();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //remove chose number
+            ListNumber.Remove(Convert.ToInt32(lb1.SelectedItem));
+            //select the current number
+            chosen = Convert.ToInt32(lb1.SelectedItem);
+            ///clear listbox and load list
+            lb1.Items.Clear();
+            for(int i=0;i<ListNumber.Count;i++)
+            {
+                lb1.Items.Add(ListNumber[i]);
+            }
+
+            //compare the number
+            if(chosen>secretNumber)
+            {
+                lb3.Text = "You choose" + chosen + ".Choose a smaller number";
+                guesses++;
+            }
+            else if(chosen==secretNumber)
+            {
+                lb3.Text = "You choose" + chosen + ".You are absolutely correct!!!";
+                lb2.Items.Add(guesses);
+                lb1.Enabled = false;
+                btnPlay.Enabled = true;
+                btnExit.Enabled = true;
+
+            }
+            else
+            {
+                lb3.Text = "You choose" + chosen + ".Choose a big number";
+                guesses++;
+            }
+        }
+    }
+}
